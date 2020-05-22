6bc68b5b3ce0d2134493c94bc5507c0c7667994a
Jacob Carver <jake10693@gmail.com>
2020-05-21 21:13:22 -0700

    Updates

---
+++ precache-manifest.97ff260db20fd3a6e8cf61ba387e35d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201ca5a84dd5280f5912bfa278389d94",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "371915924ad2b8c12053",
+    "url": "/React_Portfolio/static/css/main.3daf6479.chunk.css"
+  },
+  {
+    "revision": "13c2504bab4149177b11",
+    "url": "/React_Portfolio/static/js/2.56692cda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Portfolio/static/js/2.56692cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371915924ad2b8c12053",
+    "url": "/React_Portfolio/static/js/main.d6945c87.chunk.js"
+  },
+  {
+    "revision": "c443b31d4ac7e98cb737",
+    "url": "/React_Portfolio/static/js/runtime-main.a86ad42b.js"
+  },
+  {
+    "revision": "a3c40069960212a21bbc150319e3927b",
+    "url": "/React_Portfolio/static/media/click_me.a3c40069.png"
+  }
+]);
